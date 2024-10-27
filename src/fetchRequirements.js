58b01efdd0b9e54beff72f6cdb0319ac2c4f175b
Harsh Gill <harsh.gill@yahoo.com>
2024-10-27 01:22:00 -0600

    Added Calendar fetching

--- src/fetchRequirements.js
+++ src/fetchRequirements.js
@@ -23,7 +23,7 @@ const fetchRequirements = async (year, program, major) => {
         form.append('catalog', select);
         form.append('sel_cat_submit', 'GO');
 
-        const response = await axios.post('https://calendar.ualberta.ca/index.php', form, {
+        const archive = await axios.post('https://calendar.ualberta.ca/index.php', form, {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Accept': 'text/html'
@@ -31,19 +31,30 @@ const fetchRequirements = async (year, program, major) => {
             maxRedirects: 5,
             validateStatus: status => status < 400});
 
-        const $ = cheerio.load(response.data);
+        let $ = cheerio.load(archive.data);
 
-        const requirements = $().text();
+        const link = $('a.navbar')
+            .filter((i, el) => $(el).text().trim() === 'Undergraduate Programs')
+            .attr('href');
 
-        return response.data;
+        const programs = await axios.get("https://calendar.ualberta.ca" + link);
+        $ = cheerio.load(programs.data)
+        
+        const requirements_link = $('ul.program-list a')
+            .filter((i, el) => $(el).text().trim() === program)
+            .attr('href');
+
+        const requirements = await axios.get("https://calendar.ualberta.ca" + requirements_link);
+        $ = cheerio.load(requirements.data);
+
+        console.log($().text());
+
+        return archive.data;
     } catch (error) {
         console.error('Error fetching calendar:', error.message);
         throw(error);
     }
 }
 
-fetchRequirements('2022-2023', 'Science', 'Computer Science')
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
-
+fetchRequirements('2022-2023', 'Science', 'Computer Science');
 export default fetchRequirements;
